c19e0315e3825d827781e846535919a71194b147
Rick <rick@Ricks-MacBook-Pro.local>
2025-12-11 16:49:50 +0530

    feat: animate screen pet fly-in

--- src/components/ScreenPet.jsx
+++ src/components/ScreenPet.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 const ScreenPet = () => {
+    React.useEffect(() => {
+        const timer = setTimeout(() => {
+            const bird = document.getElementById('bird-container');
+            if (bird) {
+                bird.classList.add('is-active-top-right');
+            }
+        }, 500);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div id="bird-container">
             <div className="speech-bubble">Click to download resume</div>
